3fa8d9acad3e17b718981d0c6b45198a4da91acc
vescio <rafael.vescio@sensedata.com.br>
2020-01-14 20:33:30 -0300

    card and background adjustments

--- src/Card.js
+++ src/Card.js
@@ -10,21 +10,25 @@ class Card extends Component {
 					data.Team.map((team, i) => {
 						return (
 							<div className="card">
-									<img src={team.photo} alt=""/>
-									<dl>
-										<dt>Nome:</dt>
-										<dd>{team.name}</dd>								
-										<dt>Sonho grande:</dt>
-										<dd>{team.bigDream}</dd>
-										<dt>O que quero aprender:</dt>
-										<dd>{team.wannaLearn}</dd>
-										<dt>Eu daqui um ano...</dt>
-										<dd>{team.oneYearIn}</dd>
-										<dt>O que posso ensinar?</dt>
-										<dd>{team.teach}</dd>
-										<dt>Onde me encontrar?</dt>
-										<dd><a href={team.linkedin}>meu linkedin</a></dd>
-									</dl>
+									<div className="card-img">
+										<img src={team.photo} alt=""/>
+										<div className="card-name">{team.name}</div>
+									</div>
+									<div className="card-description">
+										<dl>
+											<label></label>							
+											<dt>Sonho grande:</dt>
+											<dd>{team.bigDream}</dd>
+											<dt>O que quero aprender:</dt>
+											<dd>{team.wannaLearn}</dd>
+											<dt>Eu daqui um ano...</dt>
+											<dd>{team.oneYearIn}</dd>
+											<dt>O que posso ensinar?</dt>
+											<dd>{team.teach}</dd>
+											<dt>Onde me encontrar?</dt>
+											<dd><a href={team.linkedin}>meu linkedin</a></dd>
+										</dl>
+									</div>
 							</div>
 						);
 					})

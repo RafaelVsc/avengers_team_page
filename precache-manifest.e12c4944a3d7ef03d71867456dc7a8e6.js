fd9db14cd561e7dc24a31a6843f8e11aabd85870
vescio <rafael.vescio@sensedata.com.br>
2020-01-14 15:13:44 -0300

    Updates

---
+++ precache-manifest.e12c4944a3d7ef03d71867456dc7a8e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e652fddd98d46fc2ca309d36c616ea04",
+    "url": "/avengers_team_page/index.html"
+  },
+  {
+    "revision": "844c759ae5d84f607254",
+    "url": "/avengers_team_page/static/css/main.c1504fc4.chunk.css"
+  },
+  {
+    "revision": "61cf90f2cac5efa54e21",
+    "url": "/avengers_team_page/static/js/2.f71a1042.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/avengers_team_page/static/js/2.f71a1042.chunk.js.LICENSE"
+  },
+  {
+    "revision": "844c759ae5d84f607254",
+    "url": "/avengers_team_page/static/js/main.c2134f25.chunk.js"
+  },
+  {
+    "revision": "0b9f3102b1eff76e6750",
+    "url": "/avengers_team_page/static/js/runtime-main.70f72b8a.js"
+  }
+]);

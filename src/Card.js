17ff6258b31d62a16bb8753e95cd93fd97762ad5
Leonardo Santos Severino <leonardossev@gmail.com>
2020-01-15 00:33:20 -0300

    Sets the style and shows the participant's name in the card

--- src/Card.js
+++ src/Card.js
@@ -1,40 +1,30 @@
 import React, { Component } from 'react';
-import data from "./data"; 
 import './assets/Main.css';
 
 class Card extends Component {
+  static get CARD_STYLE() {
+    return {
+      height: "600px",
+      width: "600px",
+      paddingTop: "80px",
+      textAlign: "center",
+      background: "#52C0F5",
+      color: "#FFF",
+      fontFamily: "sans-serif",
+      fontSize: "12px",
+      textTransform: "uppercase",
+      borderRadius: "10px",
+      boxSizing: "border-box"
+    };
+  }
+
 	render() {
+    const { participant } = this.props
 		return (
-            <div className="container">
-                {
-					data.Team.map((team, i) => {
-						return (
-							<div className="card">
-									<div className="card-img">
-										<img src={team.photo} alt=""/>
-										<div className="card-name">{team.name}</div>
-									</div>
-									<div className="card-description">
-										<dl>
-											<label></label>							
-											<dt>Sonho grande:</dt>
-											<dd>{team.bigDream}</dd>
-											<dt>O que quero aprender:</dt>
-											<dd>{team.wannaLearn}</dd>
-											<dt>Eu daqui um ano...</dt>
-											<dd>{team.oneYearIn}</dd>
-											<dt>O que posso ensinar?</dt>
-											<dd>{team.teach}</dd>
-											<dt>Onde me encontrar?</dt>
-											<dd><a href={team.linkedin}>meu linkedin</a></dd>
-										</dl>
-									</div>
-							</div>
-						);
-					})
-				}
-            </div>
-        );
+      <div style={Card.CARD_STYLE}>
+        <h1>{ participant.name }</h1>
+      </div>
+      );
     }
 } 
 export default Card;

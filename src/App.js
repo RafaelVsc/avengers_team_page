ec10aefe52f26dbf467da3042cc2a518f96ec57f
Leonardo Santos Severino <leonardossev@gmail.com>
2020-01-15 00:32:56 -0300

    Uses the Carousel component instead of the Card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css"
-import Card from './Card';
+import Carousel from './Carousel';
 function App() {
   return (
     <div className="contaier-page">
@@ -8,7 +8,7 @@ function App() {
         <h1>Equipe Avengers</h1>
         <p> Aqui é uma descrição do time - Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veritatis rerum odit ratione explicabo molestiae eveniet. Aliquam hic, totam enim distinctio incidunt recusandae, quia nulla odit consequatur voluptate ad sit.Laudantium aut quasi voluptas dolore autem tempora, ratione similique, praesentium ipsum, dolorum adipisci impedit? Quia nobis cum natus ut sequi eos non quidem aperiam. Ducimus, non enim. Voluptate, molestiae aliquid?</p>
       </div>
-      <Card></Card>
+      <Carousel></Carousel>
     </div>
   );
 }

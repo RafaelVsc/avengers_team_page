3c9fa1a4793494d4b38ff9cf1095e08352e09df2
vescio <rafael.vescio@sensedata.com.br>
2020-01-15 22:33:03 -0300

    more data in json, clean code and css adjustments

--- src/Card.js
+++ src/Card.js
@@ -5,26 +5,18 @@ import './assets/Main.css';
 class Card extends Component {
   static get CARD_STYLE() {
     return {
-    //   height: "600px",
-	//   width: "600px",
 		display: "flex",
 		justifyContent: "space-around",
-		paddingTop: "80px",
-		textAlign: "center",
-		background: "#52C0F5",
-		color: "#FFF",
-		fontFamily: "sans-serif",
-		fontSize: "12px",
-		textTransform: "uppercase",
-		borderRadius: "10px",
-		boxSizing: "border-box"
+		padding: "100px",
+		background: "rgba(20, 78, 153, 0.6)",
+		borderRadius: 10,
 	};
 }
 
 	render() {
     const { participant } = this.props
 		return (
-      <div style={Card.CARD_STYLE}>
+      <div className='dad-container' style={Card.CARD_STYLE}>
 				<div className="container">
 					<div className="card-img">
 						<img src={participant.photo} alt=""/>
@@ -45,45 +37,12 @@ class Card extends Component {
 							<dt>O que posso ensinar?</dt>
 							<dd>{participant.teach}</dd>
 							<dt>Onde me encontrar?</dt>
-							<dd><a href={participant.linkedin}>meu linkedin</a></dd>
+							<dd><a href={participant.linkedin} target="blank">linkedin</a></dd>
 						</dl>
 					</div>
 			</div>
       </div>
       );
-        //     <div className="container">
-        //         {
-		// 			data.Team.map((team, i) => {
-		// 				return (
-		// 					<div className="card">
-		// 							<div className="card-img">
-		// 								<img src={team.photo} alt=""/>
-		// 								<div className="avenger-name">
-		// 									<div className="card-name">{team.name}</div>
-		// 									<div className="card-stack">{team.stack}</div>
-		// 								</div>
-		// 							</div>
-		// 							<div className="card-description">
-		// 								<dl>
-		// 									<label></label>							
-		// 									<dt>Sonho grande:</dt>
-		// 									<dd>{team.bigDream}</dd>
-		// 									<dt>O que quero aprender:</dt>
-		// 									<dd>{team.wannaLearn}</dd>
-		// 									<dt>Eu daqui um ano...</dt>
-		// 									<dd>{team.oneYearIn}</dd>
-		// 									<dt>O que posso ensinar?</dt>
-		// 									<dd>{team.teach}</dd>
-		// 									<dt>Onde me encontrar?</dt>
-		// 									<dd><a href={team.linkedin}>meu linkedin</a></dd>
-		// 								</dl>
-		// 							</div>
-		// 					</div>
-		// 				);
-		// 			})
-		// 		}
-        //     </div>
-        // );
     }
 } 
 export default Card;

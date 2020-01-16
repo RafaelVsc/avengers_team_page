2cd0b7a24e989b2f2d27cb8a3cd024a3ab2742d3
vescio <rafael.vescio@sensedata.com.br>
2020-01-15 22:39:06 -0300

    removing button next

--- src/Carousel.js
+++ src/Carousel.js
@@ -39,9 +39,6 @@ export default class Carousel extends Component {
             ))
           };
         </ReactCardCarousel>
-        <div>
-          <button >Next</button>
-        </div>
       </div>
     );
   }

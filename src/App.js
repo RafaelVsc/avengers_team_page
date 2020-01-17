9f34dfc9dab7b47775a2059e2af4449a116af477
vescio <rafael.vescio@sensedata.com.br>
2020-01-16 23:57:12 -0300

    correcting text 1.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="team-description">
         <h1>Equipe Avengers</h1>
         <p> 
-          Olá! Nós somos os Avengers, mas ao contrário do que você pode estar pensando este não é nenhum filme da Marvel, é o <label>Game Experience</label> (by Gama Academy), 
+          Olá! Nós somos os Avengers, mas ao contrário do que você pode estar pensando este não é nenhum filme da Marvel, é o <label>Gama Experience</label>(by Gama Academy), 
           e os desafios podem ser muito maiores do que enfrentar um <label>Titã genocida</label>. Nossa equipe é composta  por 14 integrantes, 
           sendo dividida entre 4 hipsters, 4 hustlers, 3 hypers e 3 hackers (privilegiados SIM). Escolhemos o Rafa Vescio para ser nosso "Capitão América" (que também pode ser conhecido como O Líder). 
           Logo abaixo você vai poder conhecer mais sobre nosso time… e antes que a gente se esqueça: <label>Avengers, assemble!</label>
